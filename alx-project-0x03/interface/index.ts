8717f7af1ff83a5cd5c61bb656c4d1412919358d
Joy <muirujoybackup@gmail.com>
2025-07-23 21:40:35 +0300

    added

--- alx-project-0x03/interface/index.ts
+++ alx-project-0x03/interface/index.ts
@@ -1,13 +1,19 @@
-export interface PageRouteProps {
-  pageRoute: string;
-}
-export interface ButtonProps {
-  action: () => void;
-  buttonLabel: string;
-  buttonBackgroundColor?: string;
-}
 import { ReactNode } from "react";
 
+// From Button.tsx
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+  action?: () => void;
+}
+
+// From Layout.tsx
 export interface LayoutProps {
   children: ReactNode;
+}
+
+// From index.tsx
+export interface PageRouteProps {
+  pageRoute: string;
 }
